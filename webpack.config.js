07717bfd0407a5f5ab8764c6ba9290c352a93fba
tatyana demchuk <tatuana.demchuk@gmail.com>
2023-09-26 15:35:04 +0300

    feat: add optimisation plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -10,7 +13,16 @@ module.exports = {
     path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
+
+  stats: 'errors-only',
+  infrastructureLogging: {
+    level: 'error',
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html'),
     }),
@@ -23,17 +35,40 @@ module.exports = {
       ],
     }),
   ],
-
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        include: path.resolve(__dirname, 'src'),
+        sideEffects: true,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {targets: "ie 11"}]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+      }),
+      new CssMinimizerPlugin({
+        parallel: true,
+      }),
     ],
   },
 };
